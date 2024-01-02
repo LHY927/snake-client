0fa509e202b6e6ddafcd34f47ac2f046c6b47b4f
LHY927 <liuhaiyue0927@gmail.com>
2024-01-02 23:28:51 +0800

    update control by WASD

--- input.js
+++ input.js
@@ -1,12 +1,18 @@
 let connection;
 
 const handleUserInput = function (data) {
-  // \u0003 maps to ctrl+c input
-  if (data === '\u0003') {
-    process.exit();
-  }
-    data = data.toString().toUpperCase()
-    process.stdout.write(data + "\n")
+    // \u0003 maps to ctrl+c input
+    if (data === '\u0003') {
+      process.exit();
+    }else if (data.toString().toUpperCase() === 'W'){
+      connection.write("Move: up");
+    }else if (data.toString().toUpperCase() === 'A'){
+      connection.write("Move: left");
+    }else if (data.toString().toUpperCase() === 'S'){
+      connection.write("Move: down");
+    }else if (data.toString().toUpperCase() === 'D'){
+      connection.write("Move: right");
+    }
   };
 
 const setupInput = function (conn) {
@@ -15,7 +21,6 @@ const setupInput = function (conn) {
     stdin.setRawMode(true);
     stdin.setEncoding("utf8");
     stdin.resume();
-    conn.write("Move: left")
     stdin.on("data", data => {
         handleUserInput(data);
     })
