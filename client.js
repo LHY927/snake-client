1e39bd58af30de937d20cb0462b28012470a415c
LHY927 <liuhaiyue0927@gmail.com>
2024-01-17 08:50:36 +0800

    remove all multi input command

--- client.js
+++ client.js
@@ -13,7 +13,6 @@ const connect = function () {
   
     conn.on("connect", () => {
       conn.write("Name: SNK");
-      conn.write("Move: up")
     })
   
     return conn;
--- input.js
+++ input.js
@@ -4,18 +4,18 @@ const handleUserInput = function (data) {
     // \u0003 maps to ctrl+c input
     if (data === '\u0003') {
       process.exit();
-    }else if (data.toString().toUpperCase() === 'W'){
-      connection.write("Move: up");
-      connection.write("Say: go up");
-    }else if (data.toString().toUpperCase() === 'A'){
-      connection.write("Move: left");
-      connection.write("Say: ");
-    }else if (data.toString().toUpperCase() === 'S'){
-      connection.write("Move: down");
-      connection.write("Say: ");
-    }else if (data.toString().toUpperCase() === 'D'){
-      connection.write("Move: right");
-      connection.write("Say: ");
+    } else if (data.toString().toUpperCase() === 'W') {
+        connection.write('Move: up');
+    } else if (data.toString().toUpperCase() === 'A') {
+        connection.write('Move: left');
+    } else if (data.toString().toUpperCase() === 'S') {
+        connection.write('Move: down');
+    } else if (data.toString().toUpperCase() === 'D') {
+        connection.write('Move: right');
+    } else if (data.toString().toUpperCase() === 'Y') {
+        connection.write("Say: hello");
+    } else if (data.toString().toUpperCase() === 'P') {
+        connection.write("Say: go go");
     }
   };
 
